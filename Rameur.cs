01c7921bdc3fa135bd0870c7b8335de07c406bf7
chloetlx <61579389+chloetlx@users.noreply.github.com>
2020-03-01 22:36:14 +0100

    Add files via upload

---
+++ Rameur.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rameur : MonoBehaviour
+{
+    public bool leftpress = true;
+    public int point = 0;
+    public float timeLeft = 30F;
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft > 0)
+        {
+            float x = gameObject.transform.position.x;
+            float d = 5F;
+
+
+            if (Input.GetKeyDown(KeyCode.RightArrow) && leftpress)
+            {
+                    leftpress = false;
+                    gameObject.transform.position = new Vector3(x + d, 0, 0);
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && !leftpress)
+            {
+                    leftpress = true;
+                    gameObject.transform.position = new Vector3(x - d, 0, 0);
+                    point++;
+                    Debug.Log("score: " + point);
+                    Debug.Log("temps restant: " + timeLeft + " s.");
+            }
+        }
+        else Debug.Log("Le jeu est fini, votre score est de: " + point);
+    }
+}
