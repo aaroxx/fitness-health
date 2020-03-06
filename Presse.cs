5abb256c1ed30b7e5004307c2ffe15fb00af3c14
chloetlx <61579389+chloetlx@users.noreply.github.com>
2020-03-06 10:54:01 +0100

    Add files via upload

--- Presse.cs
+++ Presse.cs
@@ -2,27 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Presse : MonoBehaviour
 {
     public Jauge temps;
-    public GameObject text;
+    public TextMeshProUGUI lettre;
+    public TextMeshProUGUI timer;
+    public TextMeshProUGUI score;
     public int point = 0;
     public float decrementation = 0.5F;
     public float timeLeft = 30F;
 
     void Start()
     {
-        text.GetComponent<Text>().text = "[M]";
         temps = GameObject.Find("Image").GetComponent<Jauge>();
         temps.set_max(100F);
         temps.set_valeur(100F);
+        lettre.text = "M";
     }
     void Update()
     {
         timeLeft -= Time.deltaTime;
         if (timeLeft > 0)
         {
+            int t = (int)timeLeft;
+            timer.text = t.ToString() + "s";
+            score.text = "score: " + point.ToString();
             temps.set_valeur(temps.get_valeur() - decrementation);
 
             if (temps.get_valeur() <= 0F)
@@ -30,32 +36,28 @@ public class Presse : MonoBehaviour
                 point -= 2;
                 decrementation -= 0.125F;
                 temps.set_valeur(100F);
-                Debug.Log("score: " + point);
-                Debug.Log("temps restant: " + timeLeft + " s.");
+                score.color = new Color32(254, 46, 46, 255);
             }
             if (Input.GetKeyDown(KeyCode.M) && temps.get_valeur() <= 15F)
             {
                 point += 2;
                 decrementation += 0.125F;
                 temps.set_valeur(100F);
-                Debug.Log("score: " + point);
-                Debug.Log("temps restant: " + timeLeft + " s.");
+                score.color = new Color32(71, 254, 51, 255);
             }
-            else if (Input.GetKeyDown(KeyCode.M) && temps.get_valeur() <= 30F)
+            else if (Input.GetKeyDown(KeyCode.M) && temps.get_valeur() <= 40F)
             {
                 point++;
                 decrementation += 0.0625F;
                 temps.set_valeur(100F);
-                Debug.Log("score: " + point);
-                Debug.Log("temps restant: " + timeLeft + " s.");
+                score.color = new Color32(71, 254, 51, 255);
             }
             else if (Input.GetKeyDown(KeyCode.M))
             {
                 point--;
                 decrementation -= 0.0625F;
                 temps.set_valeur(100F);
-                Debug.Log("score: " + point);
-                Debug.Log("temps restant: " + timeLeft + " s.");
+                score.color = new Color32(254, 46, 46, 255);
             }
             if (decrementation <= 0.0625F)
             {
